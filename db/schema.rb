17bc3a1f35a3efcb31002d0b42c0e40135fa52ad
Heather <campbell.hf@gmail.com>
2016-03-15 16:31:31 +0000

    Added first name and surname to user signup.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315132836) do
-
-ActiveRecord::Schema.define(version: 0) do
-
+ActiveRecord::Schema.define(version: 201603151532936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +29,11 @@ ActiveRecord::Schema.define(version: 0) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
