36df93da4b9e3d7b5572dc2206a470963e6fad89
itsindigo <matthewbridgesbusiness@gmail.com>
2016-03-15 14:41:34 +0000

    fixed route conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root to: "home#index"
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
 
   # Example of regular route:
